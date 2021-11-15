5c291090c77915c5b8571def11372519c55ec4c1
Berk Forbes <berkforbes@yahoo.com>
2021-11-14 21:19:22 -0700

    array conditions

--- assets/script.js
+++ assets/script.js
@@ -11,16 +11,39 @@ function generatePassword() {
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var specialCharacters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
-    // Array placeholders
-    var resultArray = [];
-    var passwordCharacters = [];
-  
-    // Promt and Confirm variables
-    var characterCount = prompt("Your password must contain between 8-128 characters. How many characters would you like in your password?");
-    var useNumbers = confirm("Would you like your password to contain numbers?");
-    var useUppercase = confirm("Would you like your password to contain uppercase letters?");
-    var useLowercase = confirm("Would you like your password to contain lowercase letters?");
-    var useSpecialCharacters = confirm("Would you like your password to contain special characters?");
+  // Array placeholders
+  var resultArray = [];
+  var passwordCharacters = [];
+
+  // Promt and Confirm variables
+  var characterCount = prompt("Your password must contain between 8-128 characters. How many characters would you like in your password?");
+  var useNumbers = confirm("Would you like your password to contain numbers?");
+  var useUppercase = confirm("Would you like your password to contain uppercase letters?");
+  var useLowercase = confirm("Would you like your password to contain lowercase letters?");
+  var useSpecialCharacters = confirm("Would you like your password to contain special characters?");
+
+  // Conditions of the arrays
+  if (useNumbers) {
+    resultArray = resultArray.concat(numbers);
+  }
+
+  if (useUppercase) {
+    resultArray = resultArray.concat(uppercaseLetters);
+  }
+
+  if (useLowercase) {
+    resultArray = resultArray.concat(lowercaseLetters);
+  }
+
+  if (useSpecialCharacters) {
+    resultArray = resultArray.concat(specialCharacters);
+  }
+
+  // If user answers no to all questions, set it to special characters
+  if (resultArray.length === 0) {
+    resultArray = specialCharacters
+  }
+
 
 }
 
